890a90f70f091dc63ba510912b8ad66990b03921
Kaiisha05 <Kaiisha@yahoo.com>
2019-10-24 18:24:09 -0400

    typos corrected

---
+++ assets/js/java.js
@@ -0,0 +1,18 @@
+  var appURL = ["https://kaiisha05.github.io/Psychic-Game/", "https://kaiisha05.github.io/Crystal-Collector/", "https://kaiisha05.github.io/Trivia-Game/", "https://kaiisha05.github.io/Giphy/", "https://kaiisha05.github.io/Train-Schedule/",  "https://jmbodnar.github.io/Group07-Project01"];
+  var appImage = ["assets/images/crystall ball.jpeg", "assets/images/crystals.jpeg", "assets/images/trivia.jpg", "assets/images/vacation.jpeg", "assets/images/train.jpeg", "assets/images/WeathertoDrink.jpg"];
+  var appName = ["Psychic", "Crystal Counter", "Childhood Trivia", "Get Giphy", "Trainin' to Meet", "Weather to Drink"]
+
+
+   for (u=0; u < appURL.length; u++) {
+       var u = $("<a>");
+       u.addClass("url");
+       $(".app-container").append(u);
+
+   }
+   for (i=0; i < appImage.length; i++);
+   for (n=0; n < appName.length; n++);
+  
+
+    
+
+
