19e0774859f4844b518462d88e4ebf95a7cfd9a0
Kaiisha05 <Kaiisha@yahoo.com>
2019-10-29 18:18:55 -0400

    cards created to hold apps

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,17 +1,41 @@
-  var appURL = ["https://kaiisha05.github.io/Psychic-Game/", "https://kaiisha05.github.io/Crystal-Collector/", "https://kaiisha05.github.io/Trivia-Game/", "https://kaiisha05.github.io/Giphy/", "https://kaiisha05.github.io/Train-Schedule/",  "https://jmbodnar.github.io/Group07-Project01"];
-  var appImage = ["assets/images/crystall ball.jpeg", "assets/images/crystals.jpeg", "assets/images/trivia.jpg", "assets/images/vacation.jpeg", "assets/images/train.jpeg", "assets/images/WeathertoDrink.jpg"];
-  var appName = ["Psychic", "Crystal Counter", "Childhood Trivia", "Get Giphy", "Trainin' to Meet", "Weather to Drink"]
+  // app appArray []; /* declaring an array*/
+  // appArray = new app[4]; /*allocating array to memory*/
+  // app[] appArray = new app[4]; /*combining the two statements into one*/
 
 
-   for (u=0; u < appURL.length; u++) {
-       var u = $("<a>");
-       u.addClass("url");
-       $(".app-container").append(u);
-
-   }
-   for (i=0; i < appImage.length; i++);
-   for (n=0; n < appName.length; n++);
   
+
+  var apps = ["appURL", "appImage", "appName"]
+
+// Build Drink Display Cards //
+function makeAppCards() {
+  var appCardsHTML = '';
+
+    var appURL = ["https://kaiisha05.github.io/Psychic-Game/", "https://kaiisha05.github.io/Crystal-Collector/", "https://kaiisha05.github.io/Trivia-Game/", "https://kaiisha05.github.io/Giphy/", "https://kaiisha05.github.io/Train-Schedule/",  "https://jmbodnar.github.io/Group07-Project01"];
+    var appImage = ["assets/images/crystall ball.jpeg", "assets/images/crystals.jpeg", "assets/images/trivia.jpg", "assets/images/vacation.jpeg", "assets/images/train.jpeg", "assets/images/WeathertoDrink.jpg"];
+    var appName = ["Psychic", "Crystal Counter", "Childhood Trivia", "Get Giphy", "Trainin' to Meet", "Weather to Drink"];
+    //the $ below is not jQuery, it is template literals, which allow making strings out of variables and expressions
+
+    appCardsHTML += `
+  <div class="drink-card">
+  <h3 class="drink-card__header">${appName}<h3>
+  <div class="drink-card__body">
+  <img 
+  src="${appImage}"
+  alt="drink"
+  class="drink-card__image"
+  />
+  <div>
+  <a class="drink-card__link" href="${appURL}" target="_blank">Drink Me <i class="fa fa-external-link"></i></a>
+  
+  </div>
+  </div>
+  </div>`;
+  
+  // document.querySelector(".drinks").innerHTML = drinkCardsHTML;
+.app-container.documentHTML(appCardsHTML);
+};
+  makeAppCards();
 
     
 
